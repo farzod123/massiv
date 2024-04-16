cec15a0b1f42e7e2d00302e4f4e3355662f32b37
BFF <furkat@MacBook-Air-Furkat-2.local>
2024-04-16 13:27:56 +0500

    first commit

--- script.js
+++ script.js
@@ -73,14 +73,91 @@
     //         break;
     // }
 
-
     
-    let arr = ['damir', 'samir' ,'farzod' ,'shoxruh']
-    let name = prompt('name')
-        
-    if (arr = arr.filter(item => item !== name)) {
-    alert('имя было удалено')
-    } else {
-    alert('error')
-    }
-    console.log(arr);
+    // dz
+
+//    let arr = ['farzod','shorux','samir','laziz']
+//    let name = prompt('name')
+
+//    let idx = arr.indexOf(name)
+//    if (idx >= 0) {
+//         console.log(`${arr[idx]} was removed !`);
+//         arr.splice(idx,1)
+//    } else {
+//     alert('error')
+//    }
+   
+//    console.log(arr);
+
+
+    // 13.04
+
+    // let arr = ['damir', 'samir', 'farzod', 'shohrux', 'alex']
+
+    // let callBack = (name) => name[0] === 's'  
+    // let callBackOne = (name) => name[0] === 's'  
+    // let callBackTwo = (name) => name.length === 6
+
+    // console.log(
+
+    // //     arr.find(callBack),  // first name with letter (s) 
+    // //     arr.filter(callBackOne),  // all name with letter (s)
+    // //     arr.find(callBackTwo), 
+    // //     arr.filter((name) => name.length % 2)  // нечетные элементы (имена)
+
+    // );
+
+
+
+    // ZD
+
+    // let arr = [1,2,3,4,5]
+
+    // let total = 0
+    // arr.filter((num) => total = total + num)
+
+    // console.log(total);
+
+
+
+    // DZ
+
+
+    let arr = [1,2,4,'alex', [1,2], {}, null,true, false, undefined,]
+
+    let str = 0
+    let num = 0
+    let obj = 0
+    let boolean = 0
+    let others = 0
+
+    arr.forEach(element => {
+        if (typeof element === 'string') {
+            str++;
+        } else if (typeof element === 'number') {
+            num++;
+        }
+         else if (typeof element === 'boolean') {
+            boolean++
+        }
+        else if (typeof element === 'object') {
+            if (element === null) {
+            others++; 
+            } else {
+                obj++;
+            }
+        } else {
+            others++; 
+        }
+    });
+
+    console.log('string',str);
+    console.log('numbers',num);
+    console.log('objects',obj);
+    console.log('boolean',boolean);
+    console.log('others',others);
+
+
+
+  
+    
